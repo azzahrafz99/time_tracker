eb60ab00dfb06ca80ff617b7778c63706522f1a1
Fatimah Az-Zahra <azzahrafz@gmail.com>
2017-08-19 11:59:20 +0700

    update time tracking app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TimersDashboard from './components/TimersDashboard';
+import Helpers from './components/Helpers';
+
+ReactDOM.render(
+  <TimersDashboard />,
+    document.getElementById('content')
+);
